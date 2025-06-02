937ff42cc25c445e395a25b7abf83720e69dad25
Ricardo <ricardo_depaz@ricaldone.edu.sv>
2025-06-02 12:09:58 -0600

    Función para agregar completa

--- js/script.js
+++ js/script.js
@@ -42,3 +42,58 @@ const tabla = document.querySelector('#tabla tbody')
 
 //Llamada inicial para que se carguen los datos que vienen al servidor
 obtenerPersonas();
+
+
+
+//Agregar un nuevo registro
+const modal = document.getElementById("modal-agregar"); //Cuadro de diálogo
+const btnAgregar = document.getElementById("btnAbrirModal"); //+ para abrir
+const btnCerrar = document.getElementById("btnCerrarModal"); //X para cerrar
+
+btnAgregar.addEventListener("click", () => {
+    modal.showModal(); //Abrir el modal al hacer clic en el botón
+});
+
+btnCerrar.addEventListener("click", () => {
+    modal.close(); //Cerrar modal
+});
+
+//Agregar nuevo integrante desde el formulario
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+    e.preventDefault(); //"e" representa "submit" - Evita que el formulario se envíe
+
+    //Capturar los valores del formulario
+    const nombre = document.getElementById("nombre").value.trim();
+    const apellido = document.getElementById("apellido").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const edad = document.getElementById("edad").value.trim();
+
+    //Validación básica
+    if(!nombre || !apellido || !email || !edad){
+        alert("Complete todos los campos");
+        return; //Evitar que el formulario se envíe
+    }
+
+    //Llamar a la API para enviar el usuario
+    const respuesta = await fetch(API_URL, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({nombre, apellido, email, edad})
+    });
+
+    if (respuesta.ok){
+        alert("El registro fue agregado correctamente");
+
+        //Limpiar el formulario y cerrar el modal
+        document.getElementById("frmAgregar").reset();
+
+        modal.close();
+
+        //Recargar la tabla
+        obtenerPersonas();
+    }
+    else{
+        alert("Hubo un error al agregar");
+    }
+
+});
